da1e546dbab6a50b2d87d58597d26d4270033e57
Adam Salih <salih.max@gmail.com>
2022-03-28 10:49:02 +0200

    Add base64 encoding and decoding

---
+++ src/common/Base64.h
@@ -0,0 +1,18 @@
+//
+// Created by user2859193 and ryyst https://stackoverflow.com/questions/342409/how-do-i-base64-encode-decode-in-c
+//
+
+#ifndef BASE64_H
+#define BASE64_H
+
+#ifdef BUILD_FOR_LIBRARY
+
+#include <stdint.h>
+#include <stdlib.h>
+
+char *base64_encode(const unsigned char *data, size_t input_length, size_t *output_length);
+unsigned char *base64_decode(const char *data, size_t input_length, size_t *output_length);
+
+#endif
+
+#endif
