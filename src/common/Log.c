5c6400b65de4c77695b1a902b047bab44b454cd4
Adam Salih <salih.max@gmail.com>
2022-03-28 10:52:35 +0200

    Add VERBOSE flag for debuging

--- src/common/Log.c
+++ src/common/Log.c
@@ -8,11 +8,13 @@
 #include "Log.h"
 
 void _log(char *message, ...) {
+    #ifdef VERBOSE
     va_list argptr;
     va_start(argptr, message);
     vfprintf(stderr, message, argptr);
     va_end(argptr);
     printf("\n");
+    #endif
 }
 
 void _error(int code, char *message, ...) {
