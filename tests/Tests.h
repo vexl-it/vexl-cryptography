a3bd513162b0caeb2b138ec8e0c8fe52d58e45a4
karolinaklepackova <klepackova.karolina@gmail.com>
2022-05-06 01:24:39 +0200

    Test

--- tests/Tests.h
+++ tests/Tests.h
@@ -17,6 +17,7 @@ void test_curve_performance(Curve curve);
 void test_aes();
 void test_hmac();
 void test_ecies(Curve curve);
+void test_incorrect_keys_ecies(Curve curve);
 void test_ecdsa(Curve curve);
 void test_sha();
 
