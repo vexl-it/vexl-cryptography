29661b1db9b37cf5eddd3370ce64bb476303a8c5
Adam Salih <salih.max@gmail.com>
2022-08-18 10:36:19 +0200

    switch to AES gcm

--- src/AES.c
+++ src/AES.c
@@ -20,7 +20,7 @@ char *aes_decrypt(const char *password, const char *cipher) {
 
 void _aes_encrypt(const char *password, const int password_len, const char *message, const int message_len, char **cipher, int *cipher_len) {
     const EVP_MD *md = EVP_sha1();
-    const EVP_CIPHER *evp_cipher = EVP_aes_256_cbc();
+    const EVP_CIPHER *evp_cipher = EVP_aes_256_gcm();
     size_t ke_len = EVP_CIPHER_key_length(evp_cipher) + EVP_CIPHER_iv_length(evp_cipher);
     size_t km_len = EVP_MD_block_size(md);
     unsigned char ke_km[ke_len+km_len];
@@ -88,7 +88,7 @@ void _aes_decrypt(const char *password, const int password_len, const char *base
 
 
     const EVP_MD *md = EVP_sha1();
-    const EVP_CIPHER *evp_cipher = EVP_aes_256_cbc();
+    const EVP_CIPHER *evp_cipher = EVP_aes_256_gcm();
     size_t ke_len = EVP_CIPHER_key_length(evp_cipher) + EVP_CIPHER_iv_length(evp_cipher);
     size_t km_len = EVP_MD_block_size(md);
     unsigned char ke_km[ke_len + km_len];
